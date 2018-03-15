a89942460e0cd96a3289be9ec5f32a816311c05b
liulin <liulindatoucong@163.com>
2018-03-15 17:30:33 +0800

    test mybatis connect

---
+++ bigscreen/src/test/java/com/soundmagic/www/bigscreen/mybatis/TestMybatisConn.java
@@ -0,0 +1,20 @@
+package com.soundmagic.www.bigscreen.mybatis;
+
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+public class TestMybatisConn {
+	
+	@Test
+	public void testMybatisOracle() throws Exception
+	{
+		String resource = "/src/main/resource/mybatis/mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		
+	}
+}
